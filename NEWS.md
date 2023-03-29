3ab840c75ad09c0b32cf08f3593ed6bec731232e
Bill Denney <wdenney@humanpredictions.com>
2023-03-29 15:10:02 +0000

    version 0.2.0

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,26 @@
-# babelmixr2
+# nlmixr2lib
+
+# Version 0.2.0
+
+* Work with the new `rxode2` version 2.0.12 `model()` and `ini()` assignment
+  methods.
+* Therapeutic-area specific models have begun being added.
+* Models can now give the user some additional information load via the 
+  `message` meta-data.
+* Models can now be in different directories.  The change is for ease of
+  maintaining the library, it is not a change that affects users.
+* A regression where `addEta()` did not change the parameter, related to a
+  change in `rxode2`, was fixed.
+* `addEta()` detects where to add etas more robustly when covariates are on the
+  parameter.
+
+## Models added
+
+* Add Davda (2014) mAb consensus model
+* Add Liu (2017) time-dependent clearance model based on nivolumab
+* Add Kovalenko (2020) dupilumab PK model
+* Add Soehoel (2022) tralokinumab PK model
+* Add Zhu (2017) lebrikizumab PK model
 
 # Version 0.1.0
 
--- R/addEta.R
+++ R/addEta.R
@@ -1,3 +1,16 @@
+.getVarLhs <- function(model) {
+  if (!inherits(model, "rxUi")) {
+    .ui <- nlmixr2est::nlmixr2(model)
+  } else {
+    .ui <- model
+  }
+  .varLhs <- .ui$varLhs
+  # getSplitMuModel requires nlmixr2est, so the model is parsed from there...
+  # This will add the S3 method to allow $getSplitModel to work
+  if (is.null(.varLhs)) .varLhs <- .ui$getSplitMuModel$pureMuRef
+  .varLhs
+}
+
 #' Add random effects to a model
 #'
 #' @param model The model as a function
@@ -5,18 +18,18 @@
 #'   referred to as inter-individual variability, IIV) on
 #' @return The model with eta added to the requested parameters
 #' @examples
+#' library(rxode2)
 #' readModelDb("PK_1cmt") %>% addEta("ka")
 #' @export
 addEta <- function(model, eta) {
+  mod <- model # save to apply everything later
   if (is.character(eta)) {
     # Assign a default value
     eta <- stats::setNames(rep(0.1, length(eta)), eta)
   }
   checkmate::assert_numeric(eta, lower = 0, null.ok = FALSE, min.len = 1)
   # Get the mu-referenced parameter names
-  # getSplitMuModel requires nlmixr2est, so the model is parsed from there...
-  # This will add the S3 method to allow $getSplitModel to work
-  murefNames <- nlmixr2est::nlmixr2(model)$getSplitMuModel$pureMuRef
+  murefNames <- .getVarLhs(model)
   for (currentEta in names(eta)) {
     if (currentEta %in% names(murefNames)) {
       # do nothing
@@ -24,6 +37,9 @@ addEta <- function(model, eta) {
       # Remap the parameter to the mu-referenced value for modification
       priorEta <- currentEta
       currentEta <- names(murefNames)[murefNames %in% currentEta]
+      if (length(currentEta) > 1) {
+        currentEta <- currentEta[1]
+      }
       names(eta)[names(eta) == priorEta] <- currentEta
       cli::cli_alert(sprintf("Adding eta to %s instead of %s due to mu-referencing", currentEta, priorEta))
     }
@@ -34,13 +50,15 @@ addEta <- function(model, eta) {
         replace = sprintf("%s + eta%s", currentEta, currentEta)
       )
   }
-  etaIni <- lapply(X = paste0("eta", names(eta), "~", eta), FUN = stats::as.formula)
+  etaIni <- lapply(X = paste0("eta", names(eta), "~", eta), FUN = base::str2lang)
   iniArgs <-
     append(
       list(model), etaIni
     )
   # Work around rxode2 issue #277
   lotri <- rxode2::lotri
-  # Return the function itself
-  do.call(rxode2::ini, iniArgs)$fun
+  # Return the function itself or the updated ui
+  fun <- do.call(rxode2::ini, iniArgs)$fun
+  rxode2::rxode2(mod) <- body(fun)
+  mod
 }
